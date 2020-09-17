439aebdf0dfd1326d1adf34b0a6a43ffb2294200
fabricio <fabricio.jacob@outlook.com>
2020-09-17 10:36:10 -0300

    add

---
+++ src/test/java/com/github/fabriciolfj/knote/KnoteApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.fabriciolfj.knote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class KnoteApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
